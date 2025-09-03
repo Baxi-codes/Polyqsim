b50b8e55b921c8677492078afc6efccc7fe76ac7
Baxi-codes <baxidhairya2312@gmail.com>
2025-09-04 00:05:46 +0530

    Fix includes
    Signed-off-by: Baxi-codes <baxidhairya2312@gmail.com>

--- polyqsim-opt/polyqsim-opt.cpp
+++ polyqsim-opt/polyqsim-opt.cpp
@@ -6,26 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/IR/Dialect.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassManager.h"
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/InitLLVM.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/ToolOutputFile.h"
 
 #include "Polyqsim/PolyqsimDialect.h"
 #include "Polyqsim/PolyqsimOpsDialect.cpp.inc"
 
 int main(int argc, char **argv) {
-  mlir::registerAllPasses();
-  // TODO: Register polyqsim passes here.
-
   mlir::DialectRegistry registry;
   registry.insert<mlir::polyqsim::PolyqsimDialect>();
   registry.insert<mlir::func::FuncDialect>();
